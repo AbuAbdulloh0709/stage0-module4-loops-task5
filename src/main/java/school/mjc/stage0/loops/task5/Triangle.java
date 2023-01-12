d8867a104494ae9511eb32426548f6ff58c229b6
Muslimbek Soliyev <abuabdulloh0709@gmail.com>
2023-01-12 11:22:26 +0500

    module4 task5 loops

--- src/main/java/school/mjc/stage0/loops/task5/Triangle.java
+++ src/main/java/school/mjc/stage0/loops/task5/Triangle.java
@@ -2,35 +2,15 @@ package school.mjc.stage0.loops.task5;
 
 public class Triangle {
     public void printTriangle(int cathetusLength) {
-        {    String expected = """
-                8 \s
-                88\s
-                888
-                """;
-        String expected2 = """
-                8
-                88
-                888
-                8888
-                88888
-                """;
-        if (cathetusLength == 5) {
-            System.out.println(expected2);
-        } else {
-            System.out.println(expected);
-        }
-    }
-
-        String x = "";
-        for (int i = 1; i <= cathetusLength; i++) {
-            for (int j = 1; j <= cathetusLength; j++) {
+        for (int i = 0; i < cathetusLength; i++) {
+            for (int j = 0; j < cathetusLength; j++) {
                 if (i >= j) {
-                    x+="8";
+                    System.out.print("8");
                 } else {
+                    System.out.print(" ");
                 }
             }
-            x+="\n";
+            System.out.println();
         }
-        System.out.println(x);
     }
 }
